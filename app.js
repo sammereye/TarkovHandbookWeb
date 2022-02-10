94bbf7f9403cc018393f2cc500eb7ce8784b2f6f
Sammer Aburumman <sahabur@iu.edu>
2022-02-10 13:52:03 -0500

    Added base files from TarkovHandbook

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const path = require('path')
+const exphbs = require('express-handlebars');
+
 const app = express();
 const port = 3002;
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('.hbs', exphbs.engine({ extname: '.hbs' }))
+
+app.set('view engine', 'hbs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/tarkov', express.static(path.join(__dirname, 'public')));
+
 app.get('/tarkov', (req, res) => {
-  res.send('Hello World!');
+  res.render('index', {title: 'Tarkov Handbook (Web)', root_path: 'tarkov'})
 })
 
 app.listen(port, () => {
